3f6386b58e5f54be27d0d13445d963585a1d3ec8
Astr-oIT <estacio.gabrielle31@gmail.com>
2025-01-11 20:53:38 +0800

    New update with the design

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-// Some code thanks to @chrisgannon
-
-var select = function(s) {
-    return document.querySelector(s);
-  }
-  
-  function randomBetween(min,max)
-  {
-      var number = Math.floor(Math.random()*(max-min+1)+min);
-    
-      if ( number !== 0 ){
-        return number;
-      }else {
-        return 0.5;
-      }
-  }
-  
-  var tl = new TimelineMax();
-  
-  for(var i = 0; i < 20; i++){
-  
-    var t = TweenMax.to(select('.bubble' + i), randomBetween(1, 1.5), {
-      x: randomBetween(12, 15) * (randomBetween(-1, 1)),
-      y: randomBetween(12, 15) * (randomBetween(-1, 1)), 
-      repeat:-1,
-      repeatDelay:randomBetween(0.2, 0.5),
-      yoyo:true,
-      ease:Elastic.easeOut.config(1, 0.5)
-    })
-  
-    tl.add(t, (i+1)/0.6)
-  }
-  
-  tl.seek(50);
+document.addEventListener("DOMContentLoaded", () => {
+    let navLinks = document.querySelectorAll(".carousel .nav-link");
+    let slides = document.querySelectorAll(".carousel .slides img");
+    let overlays = document.querySelectorAll(".carousel .bar");
+    let maxZIndex = navLinks.length;
+    let easeInOutQuart = "cubic-bezier(0.77, 0, 0.175, 1)";
+    slides[0].classList.add("active");
+    navLinks[0].classList.add("active");
+    navLinks.forEach((navLink, activeIndex) => {
+      overlays[activeIndex].style.zIndex = `${navLinks.length - activeIndex}`;
+      navLink.addEventListener("click", () => {
+        // nav-link
+        navLinks.forEach(navLink => navLink.classList.remove("active"));
+        navLink.classList.add("active");
+        // slide
+        let currentSlide = document.querySelector(".carousel .slides img.active");
+        let slideFadeOut = currentSlide.animate(
+          [
+            { transform: "translateX(0)", opacity: 1 },
+            { transform: "translateX(5%)", opacity: 0 }
+          ],
+          {
+            duration: 600,
+            easing: "ease-in",
+            fill: "forwards"
+          }
+        );
+        slideFadeOut.onfinish = () => {
+          slides.forEach(slide => slide.classList.remove("active"));
+          let activeSlide = slides[activeIndex];
+          activeSlide.classList.add("active");
+          activeSlide.animate(
+            [
+              {
+                transform: "translateX(-5%)",
+                opacity: 0
+              },
+              {
+                transform: "translateX(0)",
+                opacity: 1
+              }
+            ],
+            { duration: 600, easing: "ease-out", fill: "forwards" }
+          );
+        };
+        // overlay
+        maxZIndex += 1;
+        let activeOverlay = overlays[activeIndex];
+        activeOverlay.style.zIndex = `${maxZIndex}`;
+        activeOverlay.style.zIndex = `${maxZIndex}`;
+        activeOverlay.animate(
+          [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }],
+          { duration: 1200, fill: "forwards", easing: easeInOutQuart }
+        );
+      });
+    });
+  });
